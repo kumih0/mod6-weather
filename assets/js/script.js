246620fa74d191f194a5da7785e0f527323834b4
KT <132851569+kumih0@users.noreply.github.com>
2023-05-31 09:38:19 -0500

    form submit event not click oops

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,21 @@ var cityName = document.getElementById("city-name");
 var dateToday = document.getElementById("date-today");
 var cityLat;
 var cityLon;
-var q;    
+var q = "";    
 var appid = "&appid=1f009ad3e6df93960048fd13eb3d2cc2";
+var temp;
+var wind;
+var humidity;
+
 function getCoords(geocodeURL){
 // creating modular fetch request url, location city name can be open for user input.
 // if no user input to draw from, will default to chicago
-    if (q !== null) {
-        cityName.textContent = q - "q=";
-        geocodeURL = geocodeURL + q + "&limit=1" + appid;
-    } else{
+    if (q == "") {
         cityName.textContent = "Chicago";
         geocodeURL = geocodeURL + "q=Chicago" + "&limit=1" + appid;
+    } else{
+        cityName.textContent = q - "q=";
+        geocodeURL = geocodeURL + q + "&limit=1" + appid;
     }
     fetch(geocodeURL)
     .then(function (response) {
@@ -41,12 +45,25 @@ function newSearch() {
     getCoords();
 }
 
-searchBtn.addEventListener("click", newSearch);
+searchBtn.addEventListener("submit", newSearch);
 
 function todayForecast(currentWeatherURL) {
     currentWeatherURL = currentWeatherURL + "lat=" + cityLat + "&lon=" + cityLon + appid;
-
+    
     console.log(currentWeatherURL);
+    fetch(currentWeatherURL)
+    .then(function (response) {  
+        return response.json;
+    })
+    .then(function (data) {  
+        console.log(data);
+        var icon = data[1].icon;
+        temp = data[3].temp;
+        wind = data[5].speed;
+        humidity = data[3].humidity;
+
+
+    })
 
 }
 // endpoint 
