4fe58acebd669b78f66c806a60cad6292b782478
KT <132851569+kumih0@users.noreply.github.com>
2023-05-30 21:47:59 -0500

    added dayjs to html, global var to js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=74ccb79a6fd86730f18a556855e63521";
+var searchInput = document.getElementById("search-input");
+var currentForecast = document.getElementById("today-forecast-container");
+var cityName = document.getElementById("city-name");
+var dateToday = document.getElementById("date-today")
+
