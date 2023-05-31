b157643d7680ac5ff2ad2329eb496b464e1dd3e8
KT <132851569+kumih0@users.noreply.github.com>
2023-05-31 09:07:08 -0500

    geocodeurl as base, locate funct for getcoords

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
-var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=1f009ad3e6df93960048fd13eb3d2cc2";
+var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?";
 var searchInput = document.getElementById("search-input");
 var currentForecast = document.getElementById("today-forecast-container");
 var cityName = document.getElementById("city-name");
 var dateToday = document.getElementById("date-today");
 var cityLat;
 var cityLon;
+var q;
 
 function getCoords(geocodeURL){
-
+// creating modular fetch request url, location city name can be open for user input.
+// if no user input to draw from, will default to chicago
+    var appid = "appid=1f009ad3e6df93960048fd13eb3d2cc2";
+    if (q !== null) {
+        geocodeURL = geocodeURL + q + "&limit=1" + "&" + appid;
+    } else{
+        geocodeURL = geocodeURL + "q=Chicago" + "&limit=1" + "&"+ appid;
+    }
     fetch(geocodeURL)
     .then(function (response) {
-        console.log(response);
-
         return response.json();
     })
+    // returning data as json object/array
     .then(function (data) {
-        console.log(data);
-        
+        // attaching lat and lon values to global var and returning the reassigned values
         cityLat = data[0].lat;
         cityLon = data[0].lon;
 
@@ -26,5 +32,9 @@ function getCoords(geocodeURL){
 
 getCoords(geocodeURL);
 console.log(cityLat, cityLon);
+
+function newSearch() {
+    
+}
 
 // endpoint https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
