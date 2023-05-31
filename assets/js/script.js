62fac140b3776a7a41d617ab4b00e3e8b65e31fa
KT <132851569+kumih0@users.noreply.github.com>
2023-05-31 08:48:36 -0500

    geocoding coordinates fetch call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
-var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=74ccb79a6fd86730f18a556855e63521";
+var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=1f009ad3e6df93960048fd13eb3d2cc2";
 var searchInput = document.getElementById("search-input");
 var currentForecast = document.getElementById("today-forecast-container");
 var cityName = document.getElementById("city-name");
-var dateToday = document.getElementById("date-today")
+var dateToday = document.getElementById("date-today");
+var cityLat;
+var cityLon;
 
+function getCoords(geocodeURL){
+
+    fetch(geocodeURL)
+    .then(function (response) {
+        console.log(response);
+
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        
+        cityLat = data[0].lat;
+        cityLon = data[0].lon;
+
+        return cityLat, cityLon;
+    })
+}
+
+getCoords(geocodeURL);
+console.log(cityLat, cityLon);
+
+// endpoint https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
