13fc42c567e2a1f2d36f352ea28e73d8aa956ece
KT <132851569+kumih0@users.noreply.github.com>
2023-05-31 23:37:30 -0500

    comments to js clearing out commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,23 @@
 var geocodeBaseURL = "http://api.openweathermap.org/geo/1.0/direct";
-// var currentWeatherBaseURL = "https://api.openweathermap.org/data/2.5/weather"
-
 var searchInput = document.getElementById("search-input");
 var searchForm = document.getElementById("search-form");
 
 var currentForecast = document.getElementById("today-forecast-container");
 var todayWeatherEl = document.getElementById("today-forecast");
 var fiveDayForecast = document.getElementById("week-forecast-container");
-// var fiveDayEl = document.getElementById("week-forecast");
 
 var cityName = document.getElementById("city-name");
-// var dateToday = dayjs().format(" (M/D/YYYY)");
 var dateTodayEl = document.getElementById("date-today");
 dateTodayEl.innerText = dayjs().format(" (M/D/YYYY)");
 
-var cityLat = 41.8755616;
-var cityLon = -87.6244212;
+var cityLat = 41.8755616; //chicago default
+var cityLon = -87.6244212; //chicago default
 var q = "";    
 var appid = "&appid=1f009ad3e6df93960048fd13eb3d2cc2";
-
-// var temp;
-// var wind;
-// var humidity;
 var geocodeURL = geocodeBaseURL +"?" + "q=Chicago" + "&limit=1" + appid;
 
-// function setDate() {
-//     dateTodayEl.innerText = dateToday;
-//     return
-// }
-// setDate();
-
+getCoords(geocodeURL);
 function getCoords(geocodeURL){
-    console.log(geocodeURL);
     fetch(geocodeURL)
     .then(function (response) {
         return response.json()
@@ -41,8 +27,6 @@ function getCoords(geocodeURL){
         // attaching lat and lon values to global var and returning the reassigned values
         cityLat = data[0].lat;
         cityLon = data[0].lon;
-        console.log(cityLat, cityLon);
-        //return cityLat, cityLon;
         todayForecast();
     })
 }
@@ -50,35 +34,31 @@ function getCoords(geocodeURL){
 function makeGeoURL() {
     // creating modular fetch request url, location city name can be open for user input.
     // if no user input to draw from, will default to chicago
-        if (q == "") {
-            cityName.textContent = "Chicago";
-            geocodeURL = geocodeBaseURL +"?" + "q=Chicago" + "&limit=1" + appid;
-        } else{
-            cityName.textContent = q - "q=";
+        // if (q !== "") {
             geocodeURL = geocodeBaseURL +"?" + q + "&limit=1" + appid;
-        }
-    return geocodeURL;
+        // } else{
+        //     cityName.textContent = "Chicago";
+        //     geocodeURL = geocodeBaseURL +"?" + "q=Chicago" + "&limit=1" + appid;
+        // }      
+        getCoords(geocodeURL);
 }
 
-getCoords(geocodeURL);
+// function newSearch(event) {
+    
 
-function newSearch() {
-    if (q == "") {
-        //add error msg?
-        makeGeoURL();
-        getCoords();
-    } else {
-        q = "q=" + searchInput.textContent;
-        makeGeoURL();
-        getCoords();
-    }
-}
-
-searchForm.addEventListener("submit", newSearch);
+// }
+searchForm.addEventListener("submit", function newSearch(event) { 
+        event.preventDefault();
+        cityName = searchInput.value;
+        console.log(cityName);
+        q = "q=" + cityName;
+        console.log(q);
+        makeGeoURL(cityName, q);
+ });
 
 function todayForecast(currentWeatherURL) {
     currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial" + appid;
-
+    //fetch request with relevant updated url from getcoords funct
     fetch(currentWeatherURL)
     .then(function (response) {  
        return response.json()
@@ -99,7 +79,7 @@ function todayForecast(currentWeatherURL) {
 }
 
 
-//5 day forecast
+//5 day forecast: dynamically create new div card elements to contain data from five day forecast fetch. pre-selected relevant array indexes of data due to three hour step in api data call (midday-ish time for forecast reading)
 function getFiveDay(fiveDayURL) {
     fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial" + appid;
 
@@ -108,39 +88,34 @@ function getFiveDay(fiveDayURL) {
         return response.json()
     })
     .then(function (data) { 
-        // console.log(data);
-        // let date1 = dayjs(data.list[3].dt_txt).format("M/D/YYYY");
-
-        // let day1 = data.list[3];
-        // let day2 = data.list[11];
-        // let day3 = data.list[19];
-        // let day4 = data.list[27];
-        // let day5 = data.list[35];
+        // turned selected array indexes of data into separate array to draw from, to make it easier to utilize for loop to dynamically create elements
         var weekArray = [data.list[3], data.list[11], data.list[19], data.list[27], data.list[35]];
-
+    
+        //gets rid of old card containers
+        fiveDayForecast.innerHTML = "";
         for (let index = 0; index < weekArray.length; index++) {
-            console.log(weekArray[index]);
-            var cardDiv = document.createElement("div");
-            cardDiv.setAttribute("class", "col-2 card-body rounded border border-primary border-2 p-2");
 
+            // div card container to hold info, needed to use bootstrap auto-layout with columns
+            var cardDiv = document.createElement("div");
+            // adding relevant layout class attributes
+            cardDiv.setAttribute("class", "col-2 card-body rounded border border-primary border-2 p-2");
+            cardDiv.setAttribute("id", "card" + index);
+            // to hold forecast data
             var weekDayEl = document.createElement("div");
+            //unique id relating to place in index weekarray
             weekDayEl.setAttribute("id", "week-forecast" + index);
 
             var dayDate = document.createElement("h5");
             dayDate.setAttribute("id", "card-title" + index);
 
             var weatherIcon = document.createElement("img");
-            weatherIcon.setAttribute("class", "card-body");
             weatherIcon.setAttribute("id", "weather-icon" + index);
 
-            console.log(weekDayEl);
+            // formatting date text with dayjs pulled from data, ignoring time stamp
             dayDate.innerText = dayjs(weekArray[index].dt_txt).format("M/D/YYYY");
-
-            console.log(dayDate.innerText);
-            console.log(weatherIcon);
-
+            //pulling specific icon from data in week array index
             let icon = "https://openweathermap.org/img/wn/" + weekArray[index].weather[0].icon + ".png";
-
+            //pulling temp, wind, humidity data from data
             let temp = weekArray[index].main.temp;
             let wind = weekArray[index].wind.speed;
             let humidity = weekArray[index].main.humidity;
@@ -153,42 +128,12 @@ function getFiveDay(fiveDayURL) {
             cardDiv.appendChild(weatherIcon);
             cardDiv.appendChild(weekDayEl);
         }
-        // weekArray.forEach(index => {
-        //     console.log(weekArray[index]);
-        //     var weekDayEl = document.createElement("div");
-        //     weekDayEl.setAttribute("id", "week-forecast" + index);
-        //     var dayDate = document.createElement("h5");
-        //     dayDate.setAttribute("id", "card-title" + index);
-        //     var weatherIcon = document.createElement("img");
-        //     weatherIcon.setAttribute("id", "weather-icon" + index)
-
-        //     // var weekDayEl = document.getElementById("week-forecast");
-        //     // weekDayEl.setAttribute("id", "week-forecast" + [index]);
-        //     console.log(weekDayEl);
-
-        //     // var dayDate = document.getElementsByClassName("card-title");
-        //     // console.log(dayDate);
-        //     // dayDate[0].setAttribute("className", "card-title" + [index])
-        //     dayDate[0].innerText = dayjs(weekArray[index].dt_txt).format("M/D/YYYY");
-        //     console.log(dayDate.innerText);
-
-        //     // var weatherIcon = document.getElementById("weather-icon");
-        //     // weatherIcon.setAttribute("id", "weather-icon" + [index]);
-        //     console.log(weatherIcon);
-        //     let icon = "https://openweathermap.org/img/wn/" + weekArray[index].weather[0].icon + ".png";
-
-        //     let temp = weekArray[index].main.temp;
-        //     let wind = weekArray[index].wind.speed;
-        //     let humidity = weekArray[index].main.humidity;
-
-        //     weatherIcon.src = icon;
-        //     weekDayEl.innerText = "Temp: " + temp + "ºF" + "\r\n" + "Wind: " + wind + "mph" + "\r\n" + "Humidity: " + humidity + "%";
-
-        //     fiveDayForecast.appendChild(dayDate);
-        //     fiveDayForecast.appendChild(weatherIcon);
-        //     fiveDayForecast.appendChild(weekDayEl);
-        // });
-
      })
     
+}
+
+//saving past searches to local storage
+function searchHistory(event) {
+    event.preventDefault();
+
 }
